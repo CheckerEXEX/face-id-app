45b8481a0fb723c316b99cc33d652622de280465
“khangndit” <khangndit@gmail.com>
2020-08-13 23:38:44 +0700

    update package.json

--- App.js
+++ App.js
@@ -3,54 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { AuthContext } from "./src/common/AuthContext";
 import Routes from './src/common/Routes';
 import { Alert } from 'react-native';
-import CameraScreen from './src/common/Camera';
+import { Provider } from 'react-redux';
+import store from './src/store';
 
-function App() {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [userDtoToken, setUserDtoToken] = React.useState({});
-
-  const authContext = React.useMemo(() => {
-    return {
-      signIn: (email, password) => {
-        if (email && password) {
-          setIsLoading(false);
-          setUserDtoToken({
-            user: "Khangndit@gmail.com",
-            password: "123456?a",
-            msnv: "VN166",
-            avatar: "base64.png"
-          })
-        } else {
-          setUserDtoToken({})
-          Alert.alert(
-            'Thông báo',
-            'Vui lòng điền Email và Mật khẩu !',
-            [
-              { text: 'OK', onPress: () => console.log('OK Pressed') }
-            ],
-            { cancelable: false }
-          );
-        }
-      },
-      signOut: () => {
-        setIsLoading(false);
-        setUserDtoToken({})
-      }
-    }
-  }, [])
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  })
-
+const App = () => {
   return (
-    <AuthContext.Provider value={authContext}>
+    <Provider store={store}>
       <NavigationContainer>
-        <Routes userDtoToken={userDtoToken} />
+        <Routes />
       </NavigationContainer>
-    </AuthContext.Provider>
+    </Provider>
   );
 }
 export default App;
